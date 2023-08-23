ec091898aeb7707384142d8563cc587484d4ef16
Skul1CrowN <boat.72545@gmail.com>
2023-08-23 17:37:45 +0700

    Add: Unit test for User controller and service.

--- src/User/test/user.service.spec.ts
+++ src/User/test/user.service.spec.ts
@@ -1,18 +1,29 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { UserController } from '../user.controller';
 import { UserService } from '../user.service';
 
-describe('UserService', () => {
+describe('UsersController', () => {
+  let controller: UserController;
   let service: UserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      controllers: [UserController],
+      providers: [
+        {
+          provide: UserService,
+          useValue: {
+            getAvailableUsers: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
+    controller = module.get<UserController>(UserController);
     service = module.get<UserService>(UserService);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(controller).toBeDefined();
   });
 });
