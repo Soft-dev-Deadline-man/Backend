ee44621e0a4d001459cb77209753d97964ee40be
Rosemarries <rosemarries.404notfound@gmail.com>
2023-08-24 16:36:27 +0700

    Add: Auth Controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpException, Post } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { AuthService } from './auth.service';
+import { AuthLogin } from './dto/auth-login.dto';
 
+@ApiTags('auth')
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private readonly authService: AuthService) {}
+
+    @Post('login-google')
+    async authenicateWithGoogleOAuth(@Body() authLogin: AuthLogin) {
+        try {
+            return await this.authService.authenticateWithGoogleOAuth(authLogin);
+        }
+        catch (err) {
+            throw new HttpException('Google OAuth authentication failed', err.status);
+        }
+    }
+}
