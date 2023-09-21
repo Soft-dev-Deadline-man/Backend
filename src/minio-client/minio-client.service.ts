2a7d10dc2ee555cbdb99ce8f5690c959ed4c6bd2
Rosemarries <rosemarries.404notfound@gmail.com>
2023-09-21 14:41:32 +0700

    Add: check image file's size and require image at least 400x400px

--- src/minio-client/minio-client.service.ts
+++ src/minio-client/minio-client.service.ts
@@ -58,8 +58,20 @@ export class MinioClientService {
     }
 
     public async upload(file: BufferedFile, bucketName: string = this.bucketName){
+        // Only allow jpeg and png
         if(!(file.mimetype.includes('jpeg') || file.mimetype.includes('png'))) {
             throw new HttpException('File type not supported', HttpStatus.BAD_REQUEST);
+        }
+        
+        // Limited to 5MB
+        if(file.size > 5000000) {
+            throw new HttpException('File size too large', HttpStatus.BAD_REQUEST);
+        }
+
+        // Image Width and Height must be more than 400px
+        const dimensions = sizeOf(file.buffer);
+        if(dimensions.width < 400 || dimensions.height < 400) {
+            throw new HttpException('Image dimensions too small', HttpStatus.BAD_REQUEST);
         }
 
         const timestamp = Date.now().toString();
@@ -85,4 +97,18 @@ export class MinioClientService {
             }
         });
     }
+}
+
+function sizeOf(buffer: string | Buffer) {
+  // git image size without using any npm package
+  const head = buffer.toString('hex', 0, 4);
+  const type = head.substring(0, 2);
+  const width = head.substring(2, 4);
+  const height = head.substring(4, 6);
+  
+  return {
+    type: type,
+    width: parseInt(width, 16),
+    height: parseInt(height, 16),
+  };
 }
