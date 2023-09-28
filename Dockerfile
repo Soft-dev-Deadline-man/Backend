02678b8928ae740d61918eaefacfb4c648b7040e
Skul1CrowN <boat.72545@gmail.com>
2023-09-28 22:50:37 +0700

    Fix: Dockerfile.

--- Dockerfile
+++ Dockerfile
@@ -2,20 +2,24 @@ FROM node:18-alpine AS base
 
 RUN npm install -g pnpm
 
-WORKDIR /app
-
 FROM base AS dependencies
 
+WORKDIR /app
 COPY package.json pnpm-lock.yaml ./
 RUN pnpm install --frozen-lockfile
 
 FROM base AS build
+
+WORKDIR /app
 COPY . .
 COPY --from=dependencies /app/node_modules ./node_modules
 RUN pnpm run build
+RUN pnpm prune --prod
 
 FROM base AS deploy
+
 WORKDIR /app
 COPY --from=build /app/dist ./dist/
 COPY --from=build /app/node_modules ./node_modules
-CMD [ "pnpm", "dist/main.js" ]
+
+CMD [ "node", "dist/main.js" ]
--- src/minio-client/minio-client.service.ts
+++ src/minio-client/minio-client.service.ts
@@ -5,108 +5,137 @@ import * as crypto from 'crypto';
 
 @Injectable()
 export class MinioClientService {
-    constructor(private readonly minio: MinioService) {
-        this.logger = new Logger(MinioClientService.name);
+  constructor(private readonly minio: MinioService) {
+    this.logger = new Logger(MinioClientService.name);
 
-        const policy = {
-            Version: '2012-10-17',
-            Statement: [
-              {
-                Effect: 'Allow',
-                Principal: {
-                  AWS: ['*'],
-                },
-                Action: [
-                  's3:ListBucketMultipartUploads',
-                  's3:GetBucketLocation',
-                  's3:ListBucket',
-                ],
-                Resource: ['arn:aws:s3:::test-bucket'], // Change this according to your bucket name
-              },
-              {
-                Effect: 'Allow',
-                Principal: {
-                  AWS: ['*'],
-                },
-                Action: [
-                  's3:PutObject',
-                  's3:AbortMultipartUpload',
-                  's3:DeleteObject',
-                  's3:GetObject',
-                  's3:ListMultipartUploadParts',
-                ],
-                Resource: ['arn:aws:s3:::test-bucket/*'], // Change this according to your bucket name
-              },
-            ],
-          };
-          this.client.setBucketPolicy(
-            process.env.MINIO_BUCKET_NAME,
-            JSON.stringify(policy),
-            function (err) {
-              if (err) throw err;
-      
-              console.log('Bucket policy set');
-            },
+    const policy = {
+      Version: '2012-10-17',
+      Statement: [
+        {
+          Effect: 'Allow',
+          Principal: {
+            AWS: ['*'],
+          },
+          Action: [
+            's3:ListBucketMultipartUploads',
+            's3:GetBucketLocation',
+            's3:ListBucket',
+          ],
+          Resource: ['arn:aws:s3:::test-bucket'], // Change this according to your bucket name
+        },
+        {
+          Effect: 'Allow',
+          Principal: {
+            AWS: ['*'],
+          },
+          Action: [
+            's3:PutObject',
+            's3:AbortMultipartUpload',
+            's3:DeleteObject',
+            's3:GetObject',
+            's3:ListMultipartUploadParts',
+          ],
+          Resource: ['arn:aws:s3:::test-bucket/*'], // Change this according to your bucket name
+        },
+      ],
+    };
+    this.client.setBucketPolicy(
+      process.env.MINIO_BUCKET_NAME,
+      JSON.stringify(policy),
+      function (err) {
+        if (err) throw err;
+
+        console.log('Bucket policy set');
+      },
+    );
+  }
+
+  private readonly logger: Logger;
+  private readonly bucketName = process.env.MINIO_BUCKET_NAME;
+
+  public get client() {
+    return this.minio.client;
+  }
+
+  public async upload(
+    file: BufferedFile,
+    bucketName: string = this.bucketName,
+  ) {
+    // Only allow jpeg and png
+    if (!(file.mimetype.includes('jpeg') || file.mimetype.includes('png'))) {
+      throw new HttpException(
+        'File type not supported',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    // Limited to 5MB
+    if (file.size > 5000000) {
+      throw new HttpException('File size too large', HttpStatus.BAD_REQUEST);
+    }
+
+    // Image Width and Height must be more than 400px
+    const dimensions = sizeOf(file.buffer);
+    if (dimensions.width < 400 || dimensions.height < 400) {
+      throw new HttpException(
+        'Image dimensions too small',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    const timestamp = Date.now().toString();
+    const hashedFileName = crypto
+      .createHash('md5')
+      .update(timestamp + file.originalname)
+      .digest('hex');
+    const extension = file.originalname.substring(
+      file.originalname.lastIndexOf('.'),
+      file.originalname.length,
+    );
+    const metadata = { 'Content-type': file.mimetype };
+    const fileName = hashedFileName + extension;
+
+    this.client.putObject(
+      bucketName,
+      fileName,
+      file.buffer,
+      metadata,
+      (err, etag) => {
+        if (err) {
+          this.logger.error(err);
+          throw new HttpException(
+            'Error uploading file',
+            HttpStatus.BAD_REQUEST,
           );
+        }
+      },
+    );
+
+    return {
+      url: `${process.env.MINIO_ENDPOINT}:${process.env.MINIO_PORT}/${process.env.MINIO_BUCKET_NAME}/${fileName}`,
+    };
+  }
+
+  public async uploadMultiple(
+    files: BufferedFile[],
+    bucketName: string = this.bucketName,
+  ) {
+    let urls = [];
+    for (let file of files) {
+      urls.push(await this.upload(file, bucketName));
     }
 
-    private readonly logger: Logger;
-    private readonly bucketName = process.env.MINIO_BUCKET_NAME;
+    // Return array of urls
+    return urls;
+  }
 
-    public get client() {
-        return this.minio.client;
-    }
-
-    public async upload(file: BufferedFile, bucketName: string = this.bucketName){
-        // Only allow jpeg and png
-        if(!(file.mimetype.includes('jpeg') || file.mimetype.includes('png'))) {
-            throw new HttpException('File type not supported', HttpStatus.BAD_REQUEST);
-        }
-        
-        // Limited to 5MB
-        if(file.size > 5000000) {
-            throw new HttpException('File size too large', HttpStatus.BAD_REQUEST);
-        }
-
-        // Image Width and Height must be more than 400px
-        const dimensions = sizeOf(file.buffer);
-        if(dimensions.width < 400 || dimensions.height < 400) {
-            throw new HttpException('Image dimensions too small', HttpStatus.BAD_REQUEST);
-        }
-
-        const timestamp = Date.now().toString();
-        const hashedFileName = crypto.createHash('md5').update(timestamp + file.originalname).digest('hex');
-        const extension = file.originalname.substring(file.originalname.lastIndexOf('.'), file.originalname.length);
-        const metadata = { 'Content-type': file.mimetype };
-        const fileName = hashedFileName + extension;
-
-        this.client.putObject(bucketName, fileName, file.buffer, metadata, (err, etag) => {
-            if(err) {
-                this.logger.error(err);
-                throw new HttpException('Error uploading file', HttpStatus.BAD_REQUEST);
-            }
-        });
-
-        return { url: `${process.env.MINIO_ENDPOINT}:${process.env.MINIO_PORT}/${process.env.MINIO_BUCKET_NAME}/${fileName}` }
-    }
-
-    public async uploadMultiple(files: BufferedFile[], bucketName: string = this.bucketName) {
-        let urls = [];
-        for(let file of files) {
-            urls.push(await this.upload(file, bucketName));
-        }
-
-        // Return array of urls
-        return urls;
+  async delete(fileName: string, bucketName: string = this.bucketName) {
+    this.client.removeObject(bucketName, fileName, (err) => {
+      if (err) {
+        throw new HttpException('Error deleting file', HttpStatus.BAD_REQUEST);
       }
-
-    async delete(fileName: string, bucketName: string = this.bucketName) {
-        this.client.removeObject(bucketName, fileName, (err) => {
-            if(err) {
-                throw new HttpException('Error deleting file', HttpStatus.BAD_REQUEST);
-            }
-        });
-    }
+    });
+  }
 }
 
 function sizeOf(buffer: string | Buffer) {
@@ -115,7 +144,7 @@ function sizeOf(buffer: string | Buffer) {
   const type = head.substring(0, 2);
   const width = head.substring(2, 4);
   const height = head.substring(4, 6);
-  
+
   return {
     type: type,
     width: parseInt(width, 16),
