326ba40c9401ad8290160254c1ad51d0da425801
Skul1CrowN <boat.72545@gmail.com>
2023-10-02 17:31:03 +0700

    Fix: expose port Dockerfile.

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
 
   const config = new DocumentBuilder()
     .setTitle('Deadline_Man API')
@@ -14,7 +14,6 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-
   await app.listen(process.env.APP_PORT);
 }
 bootstrap();
