b5248848a6be8e083f5c378dc28b31fa92236eef
PuNe <57552176+worachanon226@users.noreply.github.com>
2023-10-05 15:12:11 +0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,9 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
+
+  app.enableCors();
+
   await app.listen(process.env.APP_PORT);
 }
 bootstrap();
