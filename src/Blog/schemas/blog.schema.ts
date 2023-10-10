a4875fecc6e93d940b0f72a6d368697ad3980796
Worrapon <nine6805@gmail.com>
2023-10-10 20:29:56 +0700

    merge from main

--- src/Blog/schemas/blog.schema.ts
+++ src/Blog/schemas/blog.schema.ts
@@ -1,110 +1,3 @@
-<<<<<<< HEAD
-import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Review } from 'src/review/schemas/review.schema';
-
-export enum Category {
-  BEACH = "ชายหาดและทะเล",
-  SHOP = "ช็อปปิ้ง",
-  HISTORY = "ประวัติศาสตร์ วัฒนธรรม และศาสนา",
-  ENTERTAIN = "สวนสัตว์ สวนน้ำ และสวนสนุก",
-  MEUSEUM = "พิพิธภัณฑ์ และแหล่งเรียนรู้ทางธรรมชาติ",
-}
-export class Forbidden {
-  @Prop()
-  animal: boolean;
-
-  @Prop()
-  smoke : boolean;
-
-  @Prop()
-  alcohol: boolean;
-}
-
-export class OpenTime {
-  @Prop()
-  day: string;
-
-  @Prop()
-  time: string;
-}
-
-export class EntrancePrice {
-  @Prop()
-  child: string;
-
-  @Prop()
-  adult: string;
-
-  @Prop()
-  foreign: string;
-}
-
-export class SeparateRating {
-  @Prop()
-  star: number;
-
-  @Prop()
-  quantity: number;
-}
-
-export class Contact {
-  @Prop()
-  tel: string;
-
-  @Prop()
-  website: string;
-
-  @Prop()
-  facebook: string;
-}
-
-@Schema({
-  timestamps: true,
-})
-
-export class Blog {
-  @Prop()
-  title: string;
-
-  @Prop()
-  category: Category;
-
-  @Prop()
-  openTime: OpenTime[];
-
-  @Prop()
-  entrancePrice: EntrancePrice;
-
-  @Prop()
-  address: string;
-
-  @Prop()
-  rating: number;
-
-  @Prop()
-  separateRating: SeparateRating[];
-
-  @Prop()
-  reviews: Review[];
-
-  @Prop()
-  latitude: string;
-
-  @Prop()
-  longitude: string;
-
-  @Prop()
-  forbidden: Forbidden;
-
-  @Prop()
-  contact: Contact;
-
-  @Prop()
-  images: string[];
-}
-
-export const BlogSchema = SchemaFactory.createForClass(Blog);
-=======
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Review } from 'src/review/schemas/review.schema';
 
@@ -115,20 +8,6 @@ export enum Category {
   ENTERTAIN = 'สวนสัตว์ สวนน้ำ และสวนสนุก',
   MEUSEUM = 'พิพิธภัณฑ์ และแหล่งเรียนรู้ทางธรรมชาติ',
 }
-
-export enum Days {
-  MONDAY = 'จันทร์',
-  TUESDAY = 'อังคาร',
-  WEDNESDAY = 'พุธ',
-  THURSDAY = 'พฤหัสบดี',
-  FRIDAY = 'ศุกร์',
-  SATURDAY = 'เสาร์',
-  SUNDAY = 'อาทิตย์',
-  EVERYDAY = 'ทุกวัน',
-  WORKDAY = 'วันจันทร์ - ศุกร์',
-  DAYOFF = 'เสาร์ - อาทิตย์',
-}
-
 export class Forbidden {
   @Prop()
   animal: boolean;
@@ -176,9 +55,6 @@ export class Contact {
 
   @Prop()
   facebook: string;
-
-  @Prop()
-  ig: string;
 }
 
 @Schema({
@@ -226,4 +102,3 @@ export class Blog {
 }
 
 export const BlogSchema = SchemaFactory.createForClass(Blog);
->>>>>>> 5b22fe670b22d11c6c433ed9b2bc53cfa9ed7b05
