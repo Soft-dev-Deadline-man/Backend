ece52de77d713330442a60f71a3e0619d226bd96
PuNe <worachanon226@hotmail.com>
2023-10-13 19:37:23 +0700

    update: set default user profile

--- src/User/schemas/user.schema.ts
+++ src/User/schemas/user.schema.ts
@@ -15,7 +15,11 @@ export class User {
   @Prop()
   email: string;
 
-  @Prop()
+  @Prop({
+    type: String,
+    default:
+      'https://minio.pickausername.com/picture-bucket/289f07480b8a5aec15ae3b537f58cf8f.jpg',
+  })
   profile: string;
 
   @Prop()
